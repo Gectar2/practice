dcad61f82246ba8c87ced7aa6b3cb1399a000adc
Gectar2 <kavinnikov@gmail.com>
2024-03-12 18:55:43 +0300

    password and login

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Form
 from fastapi.responses import Response
 
 
@@ -7,6 +7,11 @@ app = FastAPI()
 
 @app.get('/')
 def index_page():
-    with open('templates/login.html', 'r') as f:
+    with open('templates/login.html', 'r', encoding="utf-8") as f:
         login_page = f.read()
     return Response(login_page, media_type="text/html")
+
+
+@app.post("/login")
+def process_login_page(username: str = Form(...), password: str = Form(...)):
+    return Response(f"Ваш логин {username}, ваш пароль {password}", media_type="text/html")
